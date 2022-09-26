89cc22329d2b34b95547aeefcba36c51b834b951
kalinaprym <kalina.georgieva@prym.com>
2022-09-26 13:25:28 +0200

    Updated texts on Contact page

--- src/TextBox/TextBox.js
+++ src/TextBox/TextBox.js
@@ -9,9 +9,11 @@ export default class TextBox extends Component {
                     <article className="contact-text" title="Kontakt">
                         <h2>Kontakt / Impressum</h2>
                         <p>Kalina Georgieva Design & Development</p>
-                        <p>Email: kalinagg<span style={{display: 'none'}}>diesen Text entfernen</span>@yahoo.de<br />
-                        Web: <a href="http://meineprojekte.eu3.org">meineprojekte.eu3.org</a></p>
-                        <p>Tel: +49 176 786 420 47<br />Adresse: 81539 München, Deutschland</p>
+                        <p>Email: kalinagg<span style={{display: 'none'}}>diesen Text entfernen</span>@yahoo.de</p>
+                        <p>Tel: +49 176 786 420 47</p>
+                        <p>Adresse: 81539 München, Deutschland</p>
+                        <p>LinkedIn: <a href="https://de.linkedin.com/in/kalina-georgieva-2a365744" target="_blank">Kalina auf LinkedIn</a></p>
+                        <p>Web: <a href="https://kalinagg.github.io/meineprojekte">Meine Projekte</a></p>
                     </article>
                 </section>
             </main>
--- src/TextBoxAbout/TextBoxAbout.js
+++ src/TextBoxAbout/TextBoxAbout.js
@@ -10,7 +10,7 @@ export default class TextBoxAbout extends Component {
 						<h2>Mehr über mich</h2>
 						<p>Ich bin Kalina und liebe jede Art von Design.</p>
 						<p>Nach meiner Studium in Computer Systeme und Technologien, war für mich eine Karriere als Webdesignerin die gewünschte Fortsetzung. Ich habe mir Technologien wie HTML, CSS, Dreamweaver, Flash, Photoshop selber beigebracht und als Webdesignerin in Sofia gestartet.</p>
-						<p>In 2008 bin ich nach Deutschland umgezogen, bin Richtung Frontend Entwicklung gegangen und mich technologisch noch weiter etnwickelt. Mit Technologien wie SCSS, Javascript, Typescript, React, Angular, Bootstrap, Material UI und Jest bin ich heute sehr gut vertraut.</p>
+						<p>In 2008 bin ich nach Deutschland umgezogen, bin Richtung Frontend Entwicklung gegangen und mich technologisch noch weiter entwickelt. Mit Technologien wie SCSS, Javascript, Typescript, React, Angular, Bootstrap, Material UI und Jest bin ich heute sehr gut vertraut.</p>
 						<p>Die Arbeit als Frontend Entwicklerin hat mir viel Spaß gemacht, mir fehlte aber den Design Part - meine Leidenschaft. In den letzten Monaten bilde ich mich Richtung  UX / UI Design weiter. Ich mache online Kurse und arbeite an private Design Projekte. Ich finde Figma extrem bequem und intuitiv fürs schnelles UI Prototyping und die Nielsen Norman Group Webseite gibt mir die neuesten Informationen über jegliche UX Themen.</p>
 						<p>Ich bin auf der Suche nach neuen Herausforderungen als UX Designerin, weil UX / UI ein Feld ist, wo ich meine Kreativität entfalten kann.</p>
 					</article>
