7b2366e5d6a782841f89398b937aaa20a1441058
Kalina <kalinagg@yahoo.de>
2022-11-25 17:22:47 +0100

    Updates

---
+++ precache-manifest.c83a17c067f5e7d1c008cdc9169031c5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43bc86645182e4b11165d83e152d3390",
+    "url": "/meineprojekte/index.html"
+  },
+  {
+    "revision": "903c81cd16c5ff4618e5",
+    "url": "/meineprojekte/static/css/main.494f9eab.chunk.css"
+  },
+  {
+    "revision": "e7d07a00a1b63e02311f",
+    "url": "/meineprojekte/static/js/2.2245e863.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meineprojekte/static/js/2.2245e863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903c81cd16c5ff4618e5",
+    "url": "/meineprojekte/static/js/main.6682fafa.chunk.js"
+  },
+  {
+    "revision": "f6139e939ddda20b8b7e",
+    "url": "/meineprojekte/static/js/runtime-main.7d5cb7c2.js"
+  },
+  {
+    "revision": "1f89d470eb837341ef9076d9ff17fe2e",
+    "url": "/meineprojekte/static/media/CircleGrey.1f89d470.svg"
+  },
+  {
+    "revision": "8d29df3732723b05ed8945e377aae37a",
+    "url": "/meineprojekte/static/media/CircleLightGrey.8d29df37.svg"
+  },
+  {
+    "revision": "ecfed3aa0e89793597536ee611691c14",
+    "url": "/meineprojekte/static/media/CirclePurple.ecfed3aa.svg"
+  },
+  {
+    "revision": "48b82545ad90f6a38916937657ff945f",
+    "url": "/meineprojekte/static/media/CircleYellow.48b82545.svg"
+  },
+  {
+    "revision": "f7c0b01231ad508de5c4d59375f1d7ba",
+    "url": "/meineprojekte/static/media/apetitni-ne6ta.f7c0b012.png"
+  },
+  {
+    "revision": "7690f2f07107c0d92e7515e152bb2353",
+    "url": "/meineprojekte/static/media/bavarian-costumes.7690f2f0.png"
+  },
+  {
+    "revision": "5f9d190e62e2a5628df5cb6dd8a64fcf",
+    "url": "/meineprojekte/static/media/bread-course.5f9d190e.png"
+  },
+  {
+    "revision": "d7e5bfadcc2570fc8955613c0de6cd66",
+    "url": "/meineprojekte/static/media/calendar.d7e5bfad.jpg"
+  },
+  {
+    "revision": "047e32aebcd4a1055e7967de33ce94ba",
+    "url": "/meineprojekte/static/media/casino.047e32ae.png"
+  },
+  {
+    "revision": "b9177d8882327d50e72c3bcee3ae13f6",
+    "url": "/meineprojekte/static/media/download-icon.b9177d88.svg"
+  },
+  {
+    "revision": "f6b9c187672631cc9e1db9a7140a6b98",
+    "url": "/meineprojekte/static/media/kalina-georgieva-cv-de.f6b9c187.pdf"
+  },
+  {
+    "revision": "359ab7fea5379484634c232f5dcb24f2",
+    "url": "/meineprojekte/static/media/logo.359ab7fe.svg"
+  },
+  {
+    "revision": "e0edd553c1e7476565aece02e31106f9",
+    "url": "/meineprojekte/static/media/music-4-you.e0edd553.png"
+  },
+  {
+    "revision": "f91dc10f76870a3543cbf5430349e992",
+    "url": "/meineprojekte/static/media/soccer-live-scores.f91dc10f.png"
+  },
+  {
+    "revision": "e8a560b202c81b45ae4ba1a4c105e9b7",
+    "url": "/meineprojekte/static/media/space-of-ross.e8a560b2.png"
+  },
+  {
+    "revision": "13b3d1da2697e62b7b2bb1fa0e5541dc",
+    "url": "/meineprojekte/static/media/square-pics.13b3d1da.png"
+  },
+  {
+    "revision": "3c4d4c8bac3f595ea36e469c37214f28",
+    "url": "/meineprojekte/static/media/voicecom.3c4d4c8b.png"
+  },
+  {
+    "revision": "2f969c4aba0f34bfde03907da77c90f7",
+    "url": "/meineprojekte/static/media/yoga-logo.2f969c4a.png"
+  }
+]);
