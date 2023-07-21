10aed249dca74be61b3ee9b4a15906f8281e3df2
kalinagg <kalinagg@yahoo.de>
2023-07-21 18:47:52 +0200

    Updated text for about me

--- src/TextBoxAbout/TextBoxAbout.js
+++ src/TextBoxAbout/TextBoxAbout.js
@@ -9,19 +9,10 @@ export default class TextBoxAbout extends Component {
 				<section className="info-container">
 					<article className="info-text" title="Über mich">
 						<h2>Über mich</h2>						
-						<p>Mein Name ist Kalina und ich bin leidenschaftliche UX UI Designerin.</p>						
-						<p>
-							Ich kreiere Designs für digitale Produkte oder verbessere deren Nutzbarkeit. 
-							Das könnte eine App, ein online Shop oder ein digitales Ökosystem sein.</p>
-						<p>
-							Ein gutes digitales Produkt hat mehrere Aspekte in sich, die gut zusammen harmonieren — 
-							Inhalte, Bilder, Farben und Schriften. Es ist intuitiv zu nutzen. Es ist wie unsichtbar und trotzdem da.
-						</p>
-						<p>
-							Der Benutzer ist auf sein Werk konzentriert und blendet das Produkt aus.
-							In seiner Erinnerung bleibt ein Gefühl der Zufriedenheit und nutzt das Produkt immer wieder.							
-						</p>
-						<p className="last">So entsteht ein nachhaltiges Produkt, wovon alle profitieren.</p>			
+						<p>Mein Name ist Kalina. Ich bin leidenschaftliche UX UI Designerin und kreiere Designs für digitale Produkte oder verbessere deren Nutzbarkeit. Das könnte eine App, ein Online-Shop oder ein digitales Ökosystem sein.</p>						
+						<p>Ein gutes digitales Produkt hat mehrere Aspekte in sich, die gut zusammen harmonieren — Inhalte, Bilder, Farben und Schriften. Es ist intuitiv und einfach zu bedienen. Es ist wie unsichtbar und trotzdem da.</p>
+						<p>Der Benutzer macht gute Erfahrung bei der Nutzung des Produktes. Dadurch entsteht ein Gefühl der Zufriedenheit und er wendet das Produkt immer wieder an.</p>
+						<p className="last">Eine Gemeinschaft, die das Produkt regelmäßig einsetzt, entsteht. Durch die Nutzung wird Gewinn für den Produktinhaber generiert. Die Gemeinschaft profitiert auch, weil sie ein Teil ihrer Wünsche durch das Produkt erfüllen kann.</p>
 					</article>
 					<article className="info-text info-img-container">
 						<img src={me} className="info-img" alt="my photo" />
