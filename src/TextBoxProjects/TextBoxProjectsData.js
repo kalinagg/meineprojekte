8c9b5c782e4ed0311aded682025e5f7715902142
kalinagg <kalinagg@yahoo.de>
2024-06-19 11:14:16 +0200

    Added uCore

--- src/TextBoxProjects/TextBoxProjectsData.js
+++ src/TextBoxProjects/TextBoxProjectsData.js
@@ -1,3 +1,4 @@
+import ucoreSystes from './images/ucore-systems.png'
 import livingHome from './images/living-home.png'
 import bestBefore from './images/best-before.png'
 import squarePics from './images/square-pics.png'
@@ -6,14 +7,24 @@ import bavarianCostumes from './images/bavarian-costumes.png'
 import breadCourse from './images/bread-course.png'
 import calendar from './images/calendar.jpg'
 import spaceOfRoss from './images/space-of-ross.png'
-import casino from './images/casino.png'
-import voicecom from './images/voicecom.png'
-import music4you from './images/music-4-you.png'
-import soccer from './images/soccer-live-scores.png'
+// import casino from './images/casino.png'
+// import voicecom from './images/voicecom.png'
+// import music4you from './images/music-4-you.png'
+// import soccer from './images/soccer-live-scores.png'
+// import { useSyncExternalStore } from 'react'
 
 export const projects = [
     {
         id: 0,
+        image: ucoreSystes,
+        title: 'Assisted Living',
+        subtitle: 'Mobile and Desktop App',
+        year: '2024',
+        link: 'https://www.ucore-systems.com',
+        text: 'Eine assisted living App gestaltet und konzipiert in Figma für <a href="https://www.ucore-systems.com" target="blank">uCORE Systems GmbH</a>'
+    },
+    {
+        id: 1,
         image: livingHome,
         title: 'Living Home',
         subtitle: 'Mobile App',
@@ -22,7 +33,7 @@ export const projects = [
         text: 'Eine Smart Home App mit <a href="https://www.figma.com/file/1OoJtGnwonubyYQ8UpMcMK/Living-home?type=design&node-id=0%3A1&mode=design&t=BbU8GI8H7lOCUs5L-1" target="blank">Layouts in Figma</a> und <a href="https://www.figma.com/file/A5G35XUW0iDKr9zn7YSGnA/Kalina-Georgieva---Projektkonzept?type=whiteboard&node-id=0%3A1&t=ZMbKuLy7k4TtlHGA-1" target="blank">Design Konzept in FigJam</a>'
     },
     {
-        id: 1,
+        id: 2,
         image: bestBefore,
         title: 'Best Before',
         subtitle: 'Mobile App',
@@ -31,7 +42,7 @@ export const projects = [
         text: '<a href="https://www.figma.com/file/8xOnZBaoKKlb08ElL8FJvf/BestBefore?node-id=0%3A1&t=1f8loummLFzQ49jy-0" target="blank">Layouts in Figma</a> für eine App zum Verfolgen von Mindesthaltbarkeitsdaten'
     },
     {
-        id: 2,
+        id: 3,
         image: squarePics,
         title: 'Square Pics',
         subtitle: 'Mobile App',
@@ -40,7 +51,7 @@ export const projects = [
         text: '<a href="https://www.figma.com/file/y1EsIn7mQ2QkK1kbbkcUfl/Square-Pics?node-id=0%3A1" target="blank">Layouts in Figma</a> für eine App, die PDF Photokalender erstellt'
     },
     {
-        id: 3,
+        id: 4,
         image: apetitniNe6ta,
         title: 'apetitni.ne6ta',
         subtitle: 'Webseite',
@@ -49,7 +60,7 @@ export const projects = [
         text: 'Umgesetzt mit React, Typescript und Material UI. Checken Sie die <a href="https://apetitni-ne6ta.vercel.app" target="_blank">Webseite</a> oder den Code auf <a href="https://github.com/kalinagg/apetitni.ne6ta" target="blank">Github</a>.'
     },
     {
-        id: 4,
+        id: 5,
         image: calendar,
         title: 'Photokalender',
         year: '2018',
@@ -58,7 +69,7 @@ export const projects = [
         text: 'Gestaltung in Photoshop'
     },
     {
-        id: 5,
+        id: 6,
         image: breadCourse,
         title: 'Brotbackkurs-Gutschein',
         subtitle: 'Print',
@@ -67,7 +78,7 @@ export const projects = [
         text: 'Gestaltung in Photoshop'
     },
     {
-        id: 6,
+        id: 7,
         image: bavarianCostumes,
         title: 'Trachtenmode',
         subtitle: 'Online Shop',
@@ -76,7 +87,7 @@ export const projects = [
         text: 'Gestaltung in Photoshop'
     },
     {
-        id: 7,
+        id: 8,
         image: spaceOfRoss,
         title: 'The Space of Ross',
         subtitle: 'Webseite',
@@ -84,40 +95,40 @@ export const projects = [
         link: spaceOfRoss,
         text: 'Gestaltung in Photoshop'
     },
-    {
-        id: 9,
-        image: casino,
-        title: 'Aces Up Casino Parties',
-        subtitle: 'Webseite, Mochanin Corp.',
-        year: '2008',
-        link: casino,
-        text: 'Gestaltung in Photoshop'
-    },
-    {
-        id: 10,
-        image: voicecom,
-        title: 'Voicecom',
-        subtitle: 'Webseite, Voicecom AG',
-        year: '2007',
-        link: voicecom,
-        text: 'Gestaltung in Photoshop. Enwicklung der HTML Vorlagen in Dreamweaver'
-    },
-    {
-        id: 11,
-        image: music4you,
-        title: 'Music4you',
-        subtitle: 'Animiertes Werbebanner, Voicecom AG',
-        year: '2007',
-        link: music4you,
-        text: 'Gestaltung in Flash'
-    },
-    {
-        id: 12,
-        image: soccer,
-        title: 'Soccer',
-        subtitle: 'Animiertes Werbebanner, Voicecom AG',
-        year: '2007',
-        link: soccer,
-        text: 'Gestaltung in Flash'
-    }
+    // {
+    //     id: 9,
+    //     image: casino,
+    //     title: 'Aces Up Casino Parties',
+    //     subtitle: 'Webseite, Mochanin Corp.',
+    //     year: '2008',
+    //     link: casino,
+    //     text: 'Gestaltung in Photoshop'
+    // },
+    // {
+    //     id: 10,
+    //     image: voicecom,
+    //     title: 'Voicecom',
+    //     subtitle: 'Webseite, Voicecom AG',
+    //     year: '2007',
+    //     link: voicecom,
+    //     text: 'Gestaltung in Photoshop. Enwicklung der HTML Vorlagen in Dreamweaver'
+    // },
+    // {
+    //     id: 11,
+    //     image: music4you,
+    //     title: 'Music4you',
+    //     subtitle: 'Animiertes Werbebanner, Voicecom AG',
+    //     year: '2007',
+    //     link: music4you,
+    //     text: 'Gestaltung in Flash'
+    // },
+    // {
+    //     id: 12,
+    //     image: soccer,
+    //     title: 'Soccer',
+    //     subtitle: 'Animiertes Werbebanner, Voicecom AG',
+    //     year: '2007',
+    //     link: soccer,
+    //     text: 'Gestaltung in Flash'
+    // }
 ]
